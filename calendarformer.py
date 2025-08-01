8148b813d9c9709956da2a410614461fa34c903f
DecebalRox2 <venkata.rsb@gmail.com>
2025-07-31 17:55:36 -0700

    Update calendarformer.py
    Removed small bug

--- calendarformer.py
+++ calendarformer.py
@@ -6,21 +6,32 @@ with open("basecal.ics","r") as f1:
 with open("infocal.ics","r") as f2:
     data2 = f2.readlines()
 
-with open("calendarformer.txt","w") as f3:
+with open("transformedcal.txt","w") as f3:
     f3.write("BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:uci.edu\nX-WR-TIMEZONE:America/Los_Angeles\nBEGIN:VTIMEZONE\nTZID:America/Los_Angeles\nX-LIC-LOCATION:America/Los_Angeles\nBEGIN:DAYLIGHT\nTZOFFSETFROM:-0800\nTZOFFSETTO:-0700\nTZNAME:PDT\nDTSTART:19700308T020000\nRRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU\nEND:DAYLIGHT\nBEGIN:STANDARD\nTZOFFSETFROM:-0700\nTZOFFSETTO:-0800\nTZNAME:PST\nDTSTART:19701101T020000\nRRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU\nEND:STANDARD\nEND:VTIMEZONE")
     k = 25
-    while k < 213:
-        f3.write("\nBEGIN:VEVENT\n")
-        f3.write(data2[k]) # Adds UID
-        f3.write(data2[k+3]) # Adds start date and time
-        f3.write("SEQUENCE:0\nTRANSP:OPAQUE\n") # Prints parameters I have no clue about
-        f3.write(data2[k+4]) # Adds end date and time
-        f3.write(data2[k+6]) # Adds location
-        f3.write("SUMMARY:" + (data2[k+1].split(":"))[1].split(" ")[0] + " " + (data2[k+1].split(":"))[1].split(" ")[1] + " " + (data2[k+5].split("\\"))[0].split(":")[1] + " " + "(" + (((data2[k+1].split(":"))[1].split(" ")[2]).upper()).strip("\n") + ")") # Adds summary
-        f3.write("\nCLASS:PUBLIC\n") # Adds another parameter I have no clue about
-        f3.write(data2[k+7] if (((data2[k+1].split(":"))[1].split(" ")[2]).upper()).strip("\n") != "FINAL" else "") # Adds repetiton rule if event is not a final
-        f3.write("END:VEVENT")
-        k += 10
+    while k < len(data2) - 9:
+        if "Final" not in data2[k+1]:
+            f3.write("\nBEGIN:VEVENT\n")
+            f3.write(data2[k]) # Adds UID
+            f3.write(data2[k+3]) # Adds start date and time
+            f3.write("SEQUENCE:0\nTRANSP:OPAQUE\n") # Prints parameters I have no clue about
+            f3.write(data2[k+4]) # Adds end date and time
+            f3.write(data2[k+6]) # Adds location
+            f3.write("SUMMARY:" + ((data2[k+1].split(":"))[1].split(" ")[0]).upper() + " " + (data2[k+1].split(":"))[1].split(" ")[1] + " " + (data2[k+5].split("\\n"))[0].split(":")[1] + " " + "(" + (((data2[k+1].split(":"))[1].split(" ")[2]).upper()).strip("\n") + ")") # Adds summary
+            f3.write("\nCLASS:PUBLIC\n") # Adds another parameter I have no clue about
+            f3.write(data2[k+7]) # Adds repetiton rule if event is not a final
+            f3.write("END:VEVENT")
+            k += 10
+        else:
+            f3.write("\nBEGIN:VEVENT\n")
+            f3.write(data2[k]) # Adds UID
+            f3.write(data2[k+3]) # Adds start date and time
+            f3.write("SEQUENCE:0\nTRANSP:OPAQUE\n") # Prints parameters I have no clue about
+            f3.write(data2[k+4]) # Adds end date and time
+            f3.write(data2[k+1]) # Adds summary
+            f3.write("\nCLASS:PUBLIC\n") # Adds another parameter I have no clue about
+            f3.write("END:VEVENT")
+            k += 8
     f3.write("\nEND:VCALENDAR")
 
 # Convert txt to ics file
